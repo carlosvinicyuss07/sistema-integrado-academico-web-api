0f5d336a8f6482dd06a786e8deb2aec46fb5e3b2
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2026-04-28 07:08:19 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/carlosvinicyus/sistema_integrado_academico_web_api/SistemaIntegradoAcademicoWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlosvinicyus.sistema_integrado_academico_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaIntegradoAcademicoWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaIntegradoAcademicoWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosvinicyus/sistema_integrado_academico_web_api/SistemaIntegradoAcademicoWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosvinicyus.sistema_integrado_academico_web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaIntegradoAcademicoWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
