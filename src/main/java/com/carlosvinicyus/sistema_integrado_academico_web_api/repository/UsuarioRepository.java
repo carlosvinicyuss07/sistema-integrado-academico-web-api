48b5c5129f79f64208969ea59ee5bfd06435d0fd
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2026-05-02 22:02:00 -0300

    Feature: Camada Service - Usuario

--- src/main/java/com/carlosvinicyus/sistema_integrado_academico_web_api/repository/UsuarioRepository.java
+++ src/main/java/com/carlosvinicyus/sistema_integrado_academico_web_api/repository/UsuarioRepository.java
@@ -4,7 +4,11 @@ import com.carlosvinicyus.sistema_integrado_academico_web_api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByLogin(String login);
+
+    Optional<Usuario> findBySuapId(String suapId);
 }
